6fe5dc7cccdf41fe7acbc8ab6fd703b5d7e07a12
Steven Phan <ptviet@live.com>
2019-01-17 16:11:25 +1100

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import React, { Component } from "react";
-import Login from "./Login";
-import Register from "./Register";
 
 class App extends Component {
   render() {
     return (
       <>
-        <Login />
-        <hr />
-        <Register />
+        <h3>Hello</h3>
       </>
     );
   }
---
+++ src/auth/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Login from "./Login";
+import Register from "./Register";
+
+const Auth = () => {
+  return (
+    <>
+      <Login />
+      <hr />
+      <Register />
+    </>
+  );
+};
+
+export default Auth;
